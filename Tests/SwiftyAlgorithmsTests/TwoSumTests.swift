9c229997b5f976233b489c5ec525f02fed51e4a6
Mohshinsha Shahmadar <mohshinsha.shahmadar@ntuclink.com.sg>
2024-08-19 21:48:25 +0800

    feat: Add two sum algorithm code #1

---
+++ Tests/SwiftyAlgorithmsTests/TwoSumTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import SwiftyAlgorithms
+
+final class SwiftyAlgorithmsTests: XCTestCase {
+    func testTwoSum() throws {
+      XCTAssertEqual(twoSum([2,7,11,15], 9), [0,1])
+      XCTAssertEqual(twoSum([3,2,4], 6), [1,2])
+      XCTAssertEqual(twoSum([3,3], 6), [0,1])
+    }
+}
