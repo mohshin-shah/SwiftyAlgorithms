83cf8b0d5ac52ec63b7af8e776002b13ec0b0966
Mohshinsha Shahmadar <mohshinsha.shahmadar@ntuclink.com.sg>
2024-08-19 21:57:26 +0800

    feat: Best time to buy and sell stock to maximuize the profilt #3

---
+++ Tests/SwiftyAlgorithmsTests/BestTimeStockTests.swift
@@ -0,0 +1,10 @@
+import Foundation
+import XCTest
+@testable import SwiftyAlgorithms
+
+final class SwiftyAlgorithmsTests: XCTestCase {
+    func testTwoSum() throws {
+      XCTAssertEqual(maxProfit([7,1,5,3,6,4]), 5)
+      XCTAssertEqual(maxProfit([7,6,4,3,1]), 0)
+    }
+}
--- Tests/SwiftyAlgorithmsTests/TwoSumTests.swift
+++ Tests/SwiftyAlgorithmsTests/TwoSumTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import SwiftyAlgorithms
 
-final class SwiftyAlgorithmsTests: XCTestCase {
+final class TwoSumTests: XCTestCase {
     func testTwoSum() throws {
       XCTAssertEqual(twoSum([2,7,11,15], 9), [0,1])
       XCTAssertEqual(twoSum([3,2,4], 6), [1,2])
