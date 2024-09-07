5471f4bf4f1cb6071ceb7533f30db6f76503aff5
Mohshinsha Shahmadar <mohshinsha.shahmadar@ntuclink.com.sg>
2024-09-07 21:12:14 +0800

    feat: Add helper to manage frequency maps for Character: Int while working on string algorithms

---
+++ Sources/Dictionary+StringFrequencyMap.swift
@@ -0,0 +1,21 @@
+//
+//  Dictionary+StringFrequencyMap.swift
+//
+//
+//  Created by Mohshinsha Shahmadar on 2024-09-07.
+//
+
+import Foundation
+extension Dictionary where Key == Character, Value == Int {
+  mutating func increment(_ char: Character) {
+    self[char, default: 0] += 1
+  }
+  
+  mutating func decrement(_ char: Character) {
+    if let charCount = self[char], charCount > 1 {
+      self[char] = charCount - 1
+    } else {
+      removeValue(forKey: char)
+    }
+  }
+}
