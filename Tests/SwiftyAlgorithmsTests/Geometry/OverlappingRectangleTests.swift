85cf2241b47f36a3cd67ec778b43428c4f63f145
Mohshin Shah <mohshinshah@gmail.com>
2025-04-09 12:27:42 +0800

    Fix build failures

--- Tests/SwiftyAlgorithmsTests/Geometry/OverlappingRectangleTests.swift
+++ Tests/SwiftyAlgorithmsTests/Geometry/OverlappingRectangleTests.swift
@@ -13,19 +13,19 @@ final class OverlappingRectangleTests: XCTestCase {
 
   func testIntersectingRectangles() {
     let testCases: [AlgorithmTestCase] = [
-      .init([[0, 0, 4, 4], [2, 2, 6, 6]], true, "Partially Overlapping"),
-      .init([[0, 0, 2, 2], [3, 3, 5, 5]], false, "Non-Overlapping"),
-      .init([[0, 0, 6, 6], [2, 2, 4, 4]], true, "One Rectangle Inside Another"),
-      .init([[0, 0, 4, 4], [4, 0, 6, 3]], false, "Touching at Edge (Non-Overlapping)"),
-      .init([[0, 0, 3, 3], [3, 3, 6, 6]],  false, "Touching at Corner (Non-Overlapping)"),
-      .init([[0, 0, 2, 2], [3, 3, 4, 4]], false, "Inverted Rectangle")
+      AlgorithmTestCase([[0, 0, 4, 4], [2, 2, 6, 6]], 1, "Partially Overlapping"),
+      AlgorithmTestCase([[0, 0, 2, 2], [3, 3, 5, 5]], 0, "Non-Overlapping"),
+      AlgorithmTestCase([[0, 0, 6, 6], [2, 2, 4, 4]], 1, "One Rectangle Inside Another"),
+      AlgorithmTestCase([[0, 0, 4, 4], [4, 0, 6, 3]], 0, "Touching at Edge (Non-Overlapping)"),
+      AlgorithmTestCase([[0, 0, 3, 3], [3, 3, 6, 6]],  0, "Touching at Corner (Non-Overlapping)"),
+      AlgorithmTestCase([[0, 0, 2, 2], [3, 3, 4, 4]], 0, "Inverted Rectangle")
   ]
 
     testAlgorithm(
       name: "IntersectingRectangle",
       with: testCases
     ) { input in
-      checkIfRectanglesIntersectingEachOther(with: input)
+      return checkIfRectanglesIntersectingEachOther(with: input) ? 1 : 0
     }
   }
 }
